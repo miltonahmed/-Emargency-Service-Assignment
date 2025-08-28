34c3e3c0b75321c9db3ea9f68e37893454b38c44
Milton Ahmed <miltonahmed08@yahoo.com>
2025-08-28 22:23:13 +0400

    Some class added When the copy button is clicked, the value will be copied to the clipboard so it can be pasted anywhere, and the header section copy count will increase

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
 let hearts = 0;
+let copy = 0;
+let coins = 100;
 
 // Select all heart count display elements
 const heartDisplays = document.querySelectorAll('#heart-btn .heart_count');
@@ -15,3 +16,32 @@ document.querySelectorAll('.card_heart_icon').forEach(function (icon) {
     });
   });
 });
+
+const copyDisplays = document.querySelectorAll('#copy_btn .copy_count');
+
+document.querySelectorAll('.copy_button_card').forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    copy++;
+
+    // Update all copy count displays
+    copyDisplays.forEach(function (display) {
+      display.textContent = copy;
+    });
+
+    // Found Number 
+    let card = btn.closest('.card');
+    let number = card.querySelector('.number').textContent;
+
+    // Clipboard-এ কপি করা + alert দেখানো
+    navigator.clipboard
+      .writeText(number)
+      .then(function () {
+        alert(`✅ Copied: ${number}`);
+      })
+      
+  });
+});
+
+
+
+
